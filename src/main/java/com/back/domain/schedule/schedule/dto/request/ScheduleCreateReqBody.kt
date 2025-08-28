37771db962cc69b1ca609a321c2182f1451a5265
sso0om <ssooom142@gmail.com>
2025-08-28 12:32:49 +0900

    refactor: ScheduleCreateReqBody 코틀린 전환

---
+++ src/main/java/com/back/domain/schedule/schedule/dto/request/ScheduleCreateReqBody.kt
@@ -0,0 +1,31 @@
+package com.back.domain.schedule.schedule.dto.request
+
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
+import java.time.LocalDateTime
+
+@JvmRecord
+data class ScheduleCreateReqBody(
+    @Schema(description = "모임 ID")
+    @field:NotNull
+    val clubId: Long,
+
+    @Schema(description = "일정 제목")
+    @field:NotEmpty
+    val title: String,
+
+    @Schema(description = "일정 내용")
+    @field:NotEmpty
+    val content: String,
+
+    @Schema(description = "일정 시작일")
+    val startDate: LocalDateTime?,
+
+    @Schema(description = "일정 종료일")
+    val endDate: LocalDateTime?,
+
+    @Schema(description = "일정 장소")
+    @field:NotNull
+    val spot: String
+)
