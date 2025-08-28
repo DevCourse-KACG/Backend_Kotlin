ce285c144140a8a92e0017b306dd4c3eee1b8335
qkek123 <qkek622378@gmail.com>
2025-08-28 09:10:10 +0900

    [Refactor] config/jwt 변환 (#26)
    * JwtProperties 코틀린 변환
    
    * fix: 코드래빗 반영
    
    * fix: 클래스 레벨 조정

--- src/main/java/com/back/BackendApplication.java
+++ src/main/java/com/back/BackendApplication.java
@@ -1,12 +1,15 @@
 package com.back;
 
+import com.back.global.config.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableConfigurationProperties(JwtProperties.class)
 public class BackendApplication {
 
     public static void main(String[] args) {
