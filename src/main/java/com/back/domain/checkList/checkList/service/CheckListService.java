7ba5569628c1b26e8d0853c523ef984db8c4e4eb
CYW815 <cho0815y@gmail.com>
2025-08-29 14:53:32 +0900

    충돌 수정

--- src/main/java/com/back/domain/checkList/checkList/service/CheckListService.java
+++ src/main/java/com/back/domain/checkList/checkList/service/CheckListService.java
@@ -1,6 +1,5 @@
 package com.back.domain.checkList.checkList.service;
 
-import com.back.domain.checkList.checkList.dto.CheckListDto;
 import com.back.domain.checkList.checkList.dto.CheckListUpdateReqDto;
 import com.back.domain.checkList.checkList.dto.CheckListWriteReqDto;
 import com.back.domain.checkList.checkList.entity.CheckList;
