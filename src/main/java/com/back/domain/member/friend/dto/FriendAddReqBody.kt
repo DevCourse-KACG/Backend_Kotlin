a786d23bfc76d67f77935503ba6c770139d2e652
sso0om <ssooom142@gmail.com>
2025-08-29 14:04:19 +0900

    refactor: 코드래빗 반영

--- src/main/java/com/back/domain/member/friend/dto/FriendAddReqBody.kt
+++ src/main/java/com/back/domain/member/friend/dto/FriendAddReqBody.kt
@@ -2,12 +2,12 @@ package com.back.domain.member.friend.dto
 
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.Email
-import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.NotBlank
 
 @JvmRecord
 data class FriendAddReqBody(
     @Schema(description = "친구 요청 대상의 이메일")
-    @field:NotNull
+    @field:NotBlank
     @field:Email
     val friend_email: String
 )
