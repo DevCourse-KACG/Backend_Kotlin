ac313f4fc92b889cb14e97e0015983bf6d07244a
sso0om <ssooom142@gmail.com>
2025-08-29 11:23:31 +0900

    [Refactor] ScheduleService 코틀린 변환 (#92)
    * refactor: ScheduleService 코틀린 변환
    * refactor: 코드래빗 반영
    * refactor: 코드래빗 반영2

--- src/main/java/com/back/domain/schedule/schedule/dto/request/ScheduleUpdateReqBody.kt
+++ src/main/java/com/back/domain/schedule/schedule/dto/request/ScheduleUpdateReqBody.kt
@@ -1,25 +1,29 @@
 package com.back.domain.schedule.schedule.dto.request
 
 import io.swagger.v3.oas.annotations.media.Schema
-import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 import java.time.LocalDateTime
 
 @JvmRecord
 data class ScheduleUpdateReqBody(
     @Schema(description = "일정 제목")
-    @field:NotEmpty
+    @field:NotBlank
     val title: String,
 
     @Schema(description = "일정 내용")
-    @field:NotEmpty
+    @field:NotBlank
     val content: String,
 
     @Schema(description = "일정 시작일")
+    @field:NotNull
     val startDate: LocalDateTime,
 
     @Schema(description = "일정 종료일")
+    @field:NotNull
     val endDate: LocalDateTime,
 
     @Schema(description = "일정 장소")
+    @field:NotBlank
     val spot: String
 )
